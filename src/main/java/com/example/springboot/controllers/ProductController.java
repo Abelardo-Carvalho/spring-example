dbfc6de260deb0d79d0c87d574f1f416ee7726d7
Abelardo-Carvalho <abelardocarvalhoneto@gmail.com>
2024-02-09 17:47:18 -0300

    primeiro commit

---
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -0,0 +1,47 @@
+package com.example.springboot.controllers;
+
+import com.example.springboot.dtos.ProductModelDto;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.UUID;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProducts(@RequestBody @Valid ProductModelDto productModelDto){
+        var productModel = new ProductModel(); //instacia ProductModel
+        BeanUtils.copyProperties(productModelDto, productModel);//copia os dados do dto para o model
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel)); //O status retorna se deu certo a operação no postman.
+        // O body é o que retorna no body do postman. O metodo save não existe no ProductRepository, mas extende do JPA.
+
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value="id") UUID id){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
+}
